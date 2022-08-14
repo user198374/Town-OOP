31d8cdea1371d07f136fb76ef855d9c316500f09
user198374 <denisdeshayes@outlook.be>
2022-08-14 23:00:33 +0200

    tehc3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 from batiment import Banque, Commerce, Maison
+from personne import Worker, Employee, Criminal
 
 BNP = Banque("rue de la radinerie", 8,"BNP","GEBABEBB", "Gilbert Montagnier")
-residential_house = Maison("rue de la maison", 2, "Sanders", ["Erwann Sanders", "Théa Sanders", "Elea Sanders", "Robert Sanders"])
-commerce = Commerce("rue du commerce", 3, "alimentaire", "Jean Dujardin")
+#residential_house = Maison("rue de la maison", 2, "Sanders", ["Erwann Sanders", "Théa Sanders", "Elea Sanders", "Robert Sanders"])
+#commerce = Commerce("rue du commerce", 3, "alimentaire", "Jean Dujardin")
+BNP.call_the_cops()
