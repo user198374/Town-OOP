9fb08dec0f866a9717510c413edd29e40506413d
user198374 <denisdeshayes@outlook.be>
2022-08-14 18:27:17 +0200

    first commit

---
+++ batiment.py
@@ -0,0 +1,32 @@
+class Building:
+    def __init__(self, adress, floors):
+        self.adress = adress
+        self.floors = floors
+    
+    def get_adress(self):
+        return self.adress
+    
+    def get_floors(self):
+        return self.floors
+
+class Banque(Building):
+    def __init__(self, adress, floors, name, BIC, representative):
+        super().__init__(adress, floors)
+        self.name = name
+        self.BIC = BIC
+        self.representative = representative
+        print("La banque", name, "situé à", adress, "possède", floors, " étages . Son BIC est",BIC, " et son représentant est", representative)
+        
+class Maison(Building):
+    def __init__(self, adress, floors, family_name, occupant):
+        super().__init__(adress, floors)
+        self.family_name = family_name
+        self.occupant = occupant
+        print(f"La maison situé à {adress} possède {floors} étages et appartient à la famille {family_name}, et possède {occupant} comme occupants")
+
+class Commerce(Building):
+    def __init__(self, adress, floors, categories, holder):
+        super().__init__(self, adress, floors)
+        self.catgories = categories #type of product
+        self.holder = holder #name of the building holder
+        print(f"le commerce situé à {adress} possède {floors} et vend des marchandises de type {categories} et appartient à {holder}")        
---
+++ main.py
@@ -0,0 +1,6 @@
+
+from batiment import Banque, Commerce, Maison
+
+BNP = Banque("rue de la radinerie", 8,"BNP","GEBABEBB", "Gilbert Montagnier")
+residential_house = Maison("rue de la maison", 2, "Sanders", ["Erwann Sanders", "Théa Sanders", "Elea Sanders", "Robert Sanders"])
+commerce = Commerce("rue du commerce", 3, "alimentaire", "Jean Dujardin")
